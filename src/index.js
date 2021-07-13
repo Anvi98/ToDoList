f58fdf0043eef49ec3cb2de261f9a1a8a6fd80b1
Anvi Alex <epononanvialex@gmail.com>
2021-07-14 02:44:56 +0300

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,39 +1,37 @@
-import _ from 'lodash';
 import './style.css';
 
 const tasksList = document.querySelector('.tasksList');
 const tasks = [
   {
     index: 1,
-    description: "Wash my teeth",
-    completed: true
+    description: 'Wash my teeth',
+    completed: true,
   },
   {
     index: 2,
-    description: "Go to school",
-    completed: false
+    description: 'Go to school',
+    completed: false,
   },
   {
     index: 3,
-    description: "Sleep",
-    completed: false
-  }
+    description: 'Sleep',
+    completed: false,
+  },
 ];
 
 const showTask = (tasks) => {
-  tasks.forEach(e => {
-  const task = document.createElement('div');
-  task.innerHTML = `
+  tasks.forEach((e) => {
+    const task = document.createElement('div');
+    task.innerHTML = `
   
-  <div class="mini-section item">
-  <input class="check" type="checkbox" id="task-description" name="task-description" value="${e.index}">${e.description}
-  <i class="fas fa-ellipsis-v" style="color: gray; float: right;"></i>
-  </div>
-  `;
+    <div class="mini-section item">
+    <input class="check" type="checkbox" id="task-description" name="task-description" value="${e.index}">${e.description}
+    <i class="fas fa-ellipsis-v" style="color: gray; float: right;"></i>
+    </div>
+    `;
 
-  tasksList.appendChild(task);
-});
-
+    tasksList.appendChild(task);
+  });
 };
 
 showTask(tasks);
