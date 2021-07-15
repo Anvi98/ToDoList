d2f1d3f5f316842bd15ae369d88d1ad7ea700f2d
Anvi Alex <epononanvialex@gmail.com>
2021-07-15 20:21:32 +0300

    Add drag and drop feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import { after } from 'lodash';
 import './style.css';
 
 const tasksList = document.querySelector('.tasksList');
 const tasks = [
   {
     index: 1,
-    description: 'Wash my teeth',
+    description: 'brush my teeth',
     completed: true,
   },
   {
@@ -24,7 +25,7 @@ const showTask = (tasks) => {
     const task = document.createElement('div');
     task.innerHTML = `
   
-    <div class="mini-section item">
+    <div class="mini-section item" draggable = "true">
     <input class="check" type="checkbox" id="task-description" name="task-description" value="${e.index}">${e.description}
     <i class="fas fa-ellipsis-v" style="color: gray; float: right;"></i>
     </div>
@@ -35,3 +36,43 @@ const showTask = (tasks) => {
 };
 
 showTask(tasks);
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item =>{
+  item.addEventListener('dragstart', ()=>{
+    item.classList.add('dragging');
+
+  item.addEventListener('dragend', ()=>{
+    item.classList.remove('dragging');
+  });
+  });
+});
+
+tasksList.addEventListener('dragover', (e)=>{
+  e.preventDefault();
+  const afterElement = getDragAfterElement(tasksList, e.clientY);
+  const draggable = document.querySelector('.dragging');
+  console.log(afterElement);
+  if(afterElement == null) {
+    tasksList.appendChild(draggable);
+  } else {
+    tasksList.insertBefore(draggable, afterElement);
+  }
+
+});
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.item:not(.dragging)')];
+
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2 ;
+
+    if(offset < 0 && offset > closest.offset){
+      return {offset: offset, element: child};
+    } else {
+      return closest;
+    }
+  }, {offset: Number.NEGATIVE_INFINITY}).element;
+}
