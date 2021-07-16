1839843e67a986f24fe2805f682876285b788420
Anvi Alex <epononanvialex@gmail.com>
2021-07-16 20:25:49 +0300

    Update items object's values for index and completed keys upon user actions.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,9 @@
+import { reloadContainer, addListeners, dragOver, tasks } from './dragDrop';
 import './style.css';
-import { addListeners } from './dragDrop';
-import { dragOver } from './dragDrop';
 import { getContainer } from './status';
 
+
 const tasksList = document.querySelector('.tasksList');
-const tasks = [
-  {
-    index: 1,
-    description: 'brush my teeth',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Go to school',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'Sleep',
-    completed: false,
-  },
-];
 
 const showTask = (tasks) => {
   tasks.forEach((e) => {
@@ -30,7 +13,7 @@ const showTask = (tasks) => {
     task.setAttribute("draggable", "true");
     task.innerHTML = `
     <span>
-    <input class="check" type="checkbox" id="task-description" name="task-description" value="${e.index}">${e.description}
+    <input class="check" type="checkbox" id="task-description" name="task-description" value="${e.description}">${e.description}
     <i class="fas fa-ellipsis-v" style="color: gray; float: right;"></i>
     </span>
     `;
@@ -44,4 +27,7 @@ const items = document.querySelectorAll('.item');
 
 addListeners(items);
 dragOver(tasksList);
+reloadContainer(items);
+
+
 
