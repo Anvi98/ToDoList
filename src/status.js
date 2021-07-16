a8333ccd9f56fbfde3d3dc79cb1cc944c0c84b6f
Anvi Alex <epononanvialex@gmail.com>
2021-07-16 20:26:25 +0300

    Store the updated array of items in local storage, so the user gets the correct list values after the page reloads.

--- src/status.js
+++ src/status.js
@@ -1,12 +1,16 @@
+import { saveChanges } from "./dragDrop";
 
+// Checks if input are checked or not, and Add Or remove Css Class => The line through style.
 const getContainer = (newContainer) => {
   let d = [...newContainer.querySelectorAll("input")];
   d.forEach(input =>{
     input.addEventListener('change', (e)=>{
-      if(e.target.checked){
+      if(e.target.checked) {
         input.parentNode.classList.add('desc');
+        saveChanges();
       } else {
-        input.parentNode.classList.remove('desc');
+          input.parentNode.classList.remove('desc');
+          saveChanges();
       };
       
     });
